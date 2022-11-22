8f357b9fdaf8decdc3d854da901fd1a1e48b75c8
s-samsam225 <sameera.agarwala@gmail.com>
2022-11-22 13:22:04 -0600

    add games-data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value,int16_value,bool_value) VALUES (25086,753,1)
+
+--UPDATE sandbox SET double_value= -2.5824754 WHERE id = 125;
+
+DELETE FROM sandbox WHERE id = 100;
+
+SELECT * FROM sandbox;
